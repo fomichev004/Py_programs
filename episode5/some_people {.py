7a0ad6aa7ba8b35e93b3b9c71229f70ef0210660
527 186 <39328451+fomichev004@users.noreply.github.com>
2020-07-04 09:57:24 +0300

    Create some_people {.py

---
+++ episode5/some_people {.py
@@ -0,0 +1,18 @@
+import random
+import numpy as np
+
+#some_people = {"0" : "Петров", "1" : "Рябцев", "2" : "Табуреткин", "3" : "Скворцов", "4" : "Иванов", "5" : "Сидоров"}
+#
+#random.choice(list(some_peopled.keys())) 
+#print(some_people)
+#capital = random.choice(list(d.items()))
+#import random 
+#d = {"0" : "Петров", "1" : "Рябцев", "2" : "Табуреткин", "3" : "Скворцов", "4" : "Иванов", "5" : "Сидоров"} 
+#country, capital = random.choice(list(d.items()))
+#print(country, capital)
+
+#print(some_people["1"])
+Z = np.ones((16,16))
+k = 4
+S = np.add.reduceat(np.add.reduceat(Z, np.arange(0, Z.shape[0], k), axis=0),
+                                        np.arange(0, Z.shape[1], k), axis=1)
